805df12d5c20d48be70e83fae6a073f1a6a762aa
Varrel Marcellius <marcellius33@gmail.com>
2024-08-16 20:30:59 +0700

    update: send event reminder interval time (backend)

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -3,4 +3,4 @@
 use App\Console\Commands\SendEventReminder;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command(SendEventReminder::class)->hourly();
+Schedule::command(SendEventReminder::class)->everyFifteenMinutes();
